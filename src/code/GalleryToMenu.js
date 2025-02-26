836876e1266bc607deb4b2db36e9f329d21afc11
isracard66@gmail.com <isracard66@gmail.com>
2025-02-25 21:21:28 -0600

    Migrado de pagina que no funcionaba

---
+++ src/code/GalleryToMenu.js
@@ -0,0 +1,6 @@
+//Nombra el boton de inicio EXACTAMENTE como esto 
+const btnInicio = document.getElementById("btnInicio");
+
+btnInicio.addEventListener("click",()=>{
+    window.location.href = "../index.html"
+});
---
+++ src/code/main.js
@@ -0,0 +1,6 @@
+//Crea una constante con el nombre de tu mascota
+const GaleriaChente = document.getElementById("Chente");
+//Agrega este evento
+GaleriaChente.addEventListener("click",()=>{
+    window.location.href = "view/chente.html";
+});
